1c3ab4f8cc6090be706cebdc71f77c50beaad63a
Mcfly6 <manu.mcfly6@gmail.com>
2020-11-12 18:30:00 -0300

    Remove the now ignored directory

--- server/src/s3Uploader.ts
+++
@@ -1,9 +0,0 @@
-import { AWSS3Uploader } from "./aws/S3";
-
-//Coun\t find other option that just harcoding credentials :(
-
-export const s3Uploader = new AWSS3Uploader({
-  accessKeyId: "AKIAVRCD4A3BCZ6BMK57",
-  secretAccessKey: "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",
-  destinationBucketName: "petfinderbucker",
-});
